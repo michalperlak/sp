0401feecca303f889d4164064020ff45c5d587ec
Michał Perlak <michalperlak@windowslive.com>
2018-02-05 21:53:35 +0100

    Add timestamp to read value

--- common/src/main/kotlin/pl/edu/agh/eaiib/io/sp/common/model/Reading.kt
+++ common/src/main/kotlin/pl/edu/agh/eaiib/io/sp/common/model/Reading.kt
@@ -3,4 +3,5 @@ package pl.edu.agh.eaiib.io.sp.common.model
 data class Reading(val deviceId: String = "",
                    val accelerometer: List<Double> = emptyList(),
                    val gyroscope: List<Double> = emptyList(),
-                   val location: Location = Location())
+                   val location: Location = Location(),
+                   val timestamp: Long = System.currentTimeMillis())
